11665581a5eea38ca6aa63a51d706b513cbc8e79
Waldron63 <ingsis.gualdron.santiago@gmail.com>
2026-02-06 17:11:25 -0500

    solucion al proyecto 3

--- app.js
+++ app.js
@@ -6,7 +6,17 @@ const work_list = require('./work-list.js');
 const listViewRouter = require('./list-view-router');
 const listEditRouter = require('./list-edit-router');
 
+const HTTP_METHODS = ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'];
+
+const validateMethod = (req, res, next) => {
+    if (!HTTP_METHODS.includes(req.method)) {
+        return res.status(405).json({ error: 'Method Not Allowed' });
+    }
+    next();
+}
+
 // Use the routers
+app.use(validateMethod);
 app.use(express.json());
 app.use('/list-view', listViewRouter);
 app.use('/list-edit', listEditRouter);
--- list-view-router.js
+++ list-view-router.js
@@ -2,11 +2,30 @@ const express = require('express');
 const router = express.Router();
 const data = require('./work-list.js');
 
+const validateIdParam = (req, res, next) => {
+    const id = parseInt(req.params.id, 10);
+    if (isNaN(id)) {
+        return res.status(400).json({ error: 'Invalid ID parameter' });
+    }
+    if (id < 1) {
+        return res.status(404).json({ error: 'Item not found' });
+    }
+    next();
+}
+
+const validateIsCompleteParam = (req, res, next) => {
+    const isComplete = req.params.iscomplete;
+    if (isComplete !== 'true' && isComplete !== 'false') {
+        return res.status(400).json({ error: 'Invalid iscomplete parameter, must be true or false' });
+    }
+    next();
+}
+
 router.get('/', (req, res) => {
     res.json(data);
 })
 
-router.get('/:id', (req, res) => {
+router.get('/:id', validateIdParam, (req, res) => {
     const itemId = parseInt(req.params.id, 10);
     const item = data.find(d => d.id === itemId);
     if (item) {
@@ -16,7 +35,7 @@ router.get('/:id', (req, res) => {
     }
 })
 
-router.get('/iscomplete/:iscomplete', (req, res) => {
+router.get('/iscomplete/:iscomplete', validateIsCompleteParam, (req, res) => {
     const isComplete = req.params.iscomplete === 'true';
     const filteredItems = data.filter(d => d.iscompleted === isComplete);
     res.json(filteredItems);
